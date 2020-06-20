430c051188f83054e21daaa3c53f7ea20855b264
Brandon James <brandonj2858@gmail.com>
2020-06-20 15:34:46 -0400

    teams

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [teams, setTeams] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:3000/teams')
+    .then(res => res.json())
+    .then(resObj => setTeams(resObj))
+  })
+
   return (
     <div className="App">
       Top Scouts
