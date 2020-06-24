9870441f5a2eaa4d14c68ffbabd9c3d9071a7d82
Brandon James <brandonj2858@gmail.com>
2020-06-24 19:28:20 -0400

    pos add

--- src/App.js
+++ src/App.js
@@ -32,7 +32,19 @@ function App() {
 
       <div className="offense-container">
         <header className="offense-header">Offense</header>
-        <ul className="offense-list"> </ul>
+        <ul className="offense-list">
+        <li className="pos-item">Quarterback </li>
+        <li className="pos-item">Runningback </li>
+        <li className="pos-item">FullBack </li>
+        <li className="pos-item">Offensive Tackle</li>
+        <li className="pos-item">Offensive Gaurd </li>
+        <li className="pos-item">Tight End</li>
+        <li className="pos-item">Wide Reciever </li>
+
+
+
+
+        </ul>
       </div>
 
       </div>
