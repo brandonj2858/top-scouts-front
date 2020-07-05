77cad820b083f98edcc6913891dd48254114870f
Brandon James <brandonj2858@gmail.com>
2020-07-05 09:44:59 -0400

    display

--- src/components/PlayerForm.js
+++ src/components/PlayerForm.js
@@ -64,18 +64,26 @@ const PlayerForm = () => {
         {selectedTeam.length === 0 ? null :
           <div>
             <img className="school-logo" src={selectedTeam}/>
-            <div>
-              <li className="prof-li"> Name: {selection.first_name} {selection.last_name}</li>
-              <li className="prof-li"> Position: {selection.position}</li>
-              <li className="prof-li"> Rank: <select type="number" name="rank"  default={selection.rank}>
-                {intRange().map((num) => {return <option value={selection.rank} selected>{num}</option>})}
-              </select></li>
+            <div style={{display: "flex"}}>
+              <li className="prof-li"><span className="attributes">Name</span> <strong>{selection.first_name} {selection.last_name} </strong></li>
+              <li className="prof-li"><span className="attributes"> Position </span> <strong>{selection.position}</strong></li>
+
 
 
             </div>
 
+            <form className="update-form">
+            Rank: <select type="number" name="rank"  default={selection.rank}>
+              {intRange().map((num) => {return <option value={selection.rank} selected>{num}</option>})}
+            </select>
+            <textarea className="scout-input" type="textarea" default={selection.scouting_report} name="scouting_report"/>
+            <input type="submit" value="Submit"/>
 
-             Scouting Report: {selection.scouting_report}
+            </form>
+
+
+
+
           </div>
 
         }
