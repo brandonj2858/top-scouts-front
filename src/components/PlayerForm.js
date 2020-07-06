5b944154a502c0b95b1c50a38d4a2799d8a4e974
Brandon James <brandonj2858@gmail.com>
2020-07-06 09:09:41 -0400

    css

--- src/components/PlayerForm.js
+++ src/components/PlayerForm.js
@@ -8,6 +8,7 @@ const PlayerForm = () => {
   const [selection, setSelection] = useState([])
   const [selectedTeam, setSelectedTeam] = useState([])
   const [showProf, setShowProf] = useState(false)
+  const [teamColors, setTeamColors] = useState("")
 
   useEffect(() => {
     fetch('http://localhost:3000/teams')
@@ -45,13 +46,14 @@ const PlayerForm = () => {
       return team.school === selection.school
     })
     console.log(selectedTeam)
+    setTeamColors(teamFound[0].color)
     setSelectedTeam(teamFound[0].logos[0])
   }
 
 
 
   return (
-    <div style={{width: "70%",textAlign: "center"}}>
+    <div style={{width: "90%",textAlign: "center"}}>
 
 
 
@@ -64,9 +66,13 @@ const PlayerForm = () => {
         {selectedTeam.length === 0 ? null :
           <div>
             <img className="school-logo" src={selectedTeam}/>
-            <div style={{display: "flex"}}>
-              <li className="prof-li"><span className="attributes">Name</span> <strong>{selection.first_name} {selection.last_name} </strong></li>
-              <li className="prof-li"><span className="attributes"> Position </span> <strong>{selection.position}</strong></li>
+            <div style={{display: "flex", flexFlow: "wrap"}}>
+              <li className="prof-li"><span className="attributes">Name</span> <strong className="attributes-value" style={{color: teamColors}}>{selection.first_name} {selection.last_name} </strong></li>
+              <li className="prof-li"><span className="attributes"> Position </span> <strong className="attributes-value" style={{color: teamColors}} >{selection.position}</strong></li>
+              <li className="prof-li"><span className="attributes"> Height </span> <strong className="attributes-value" style={{color: teamColors}} >{selection.height}</strong></li>
+              <li className="prof-li"><span className="attributes"> weight </span> <strong className="attributes-value" style={{color: teamColors}} >{selection.weight}</strong></li>
+              <li className="prof-li"><span className="attributes"> School </span> <strong className="attributes-value" style={{color: teamColors}} >{selection.school}</strong></li>
+
 
 
 
