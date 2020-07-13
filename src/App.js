68b1b35cc808897a8855d0e7bffe581dc8a2856f
Brandon James <brandonj2858@gmail.com>
2020-07-13 17:48:16 -0400

    fin

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     fetch('http://newsapi.org/v2/everything?q=ncaa-football&from=2020-07-08&sortBy=publishedAt&apiKey=661358eea45c4220bca8dd46168ebcea')
       .then(res => res.json())
       .then(resObj => setNews(resObj.articles))
+      
 
     }, [news]
     )
